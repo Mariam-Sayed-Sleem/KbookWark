99e894bbf56f29849c6cd0fe2cfc881fff80c62f
Mariam Sayed Sleem <sleemsayed65@gmail.com>
2026-05-16 22:03:53 +0300

    Add files via upload

---
+++ JavaScript/Script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".slide-content", {
+      slidesPerView: 3,
+      spaceBetween: 50,
+      loop: true,
+      centerSlide: true,
+      fade: true,
+      grabCursor: true,
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        0: {slidesPerView: 1,},
+        768: {slidesPerView: 2,},
+        1024: {slidesPerView: 3,},
+      }
+    });
